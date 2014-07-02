55f66e8ebaf046aac3565b77079cff0b25ee1198
Lei Xu <eddyxu@gmail.com>
2014-07-02 04:20:21 -0500

    * use cmake ExternalPackage to install google mock / google test

--- cmake_modules/util.cmake
+++ cmake_modules/util.cmake
@@ -1,5 +1,5 @@
 function(cxx_test name libs)
 	add_executable("${name}" "${name}.cpp")
-	target_link_libraries("${name}" "${libs}")
+	target_link_libraries("${name}" "${libs}" -lpthread)
 	add_test("${name}" "${name}")
 endfunction()
